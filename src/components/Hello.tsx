1a05742c46ab756d02fb1006fae0dcebf276e0bf
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-22 22:29:32 +0300

    useState hook in TS

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 interface IProps {
     name: string;
     label: number;
     description?: string;
     onSmtHappen: (name: string) => void;
+}
+
+type User = {
+    age: number;
+    work?: 'work' | "doesn't work";
 }
 
 //first method
@@ -20,12 +25,17 @@ interface IProps {
 
 //second method
 const Hello: React.FunctionComponent<IProps> = ({ name, label }) => {
+
+    const [count, setCount] = useState<User>({ age: 43, work: 'work' })
+
     const handleChaing = (event: React.ChangeEvent<HTMLInputElement>): void => {
         console.log(event.target.value)
     }
 
     const handleClick = () => {
-        console.log('I have worked')
+        console.log('I have worked');
+        setCount({ age: 50, work: 'doesn\'t work' });
+
     }
 
     return (
