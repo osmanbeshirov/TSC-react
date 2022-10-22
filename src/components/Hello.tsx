618462e6c9dde9d78a9a3500f107e44c7ee0cb0d
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-22 21:43:47 +0300

    event types in react

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -19,14 +19,21 @@ interface IProps {
 // }
 
 //second method
-const Hello: React.FunctionComponent<IProps> = ({ name, label, onSmtHappen }) => {
+const Hello: React.FunctionComponent<IProps> = ({ name, label }) => {
+    const handleChaing = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        console.log(event.target.value)
+    }
+
+    const handleClick = () => {
+        console.log('I have worked')
+    }
+
     return (
         <div>
             <h1>Hello component {name}</h1>
             <h2>My label is: {label}</h2>
-            <>
-                {onSmtHappen(name)}
-            </>
+            <input type="text" onChange={handleChaing} />
+            <div onClick={handleClick} style={{ backgroundColor: 'purple', display: 'inline-block' }}>Click me please...</div>
 
         </div>
     )
