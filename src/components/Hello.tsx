091b211d4bfb5ba5ecd06239cb98e770fd7ae6f3
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-23 23:47:09 +0300

    second child edit

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -68,7 +68,7 @@ const Hello: React.FunctionComponent<IProps> = ({ name, label }) => {
     return (
         <div>
             {/* <h1>Hello component {name}</h1> */}
-            <h2>My label is: {label}</h2>
+            {/* <h2>My label is: {label}</h2> */}
             <input type="text" onChange={handleChaing} />
             <div ref={divRef} onClick={handleClick} style={{ backgroundColor: 'purple', display: 'inline-block' }}>Click me please...</div>
             <input type="text" ref={inputRef} />
